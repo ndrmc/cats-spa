ff4923622149a953879302c6c55e67ff01b64d36
Alexander Suraphel <alexsuraphel@gmail.com>
2017-01-20 15:29:04 +0300

    Show a basic listing of GRNs

---
+++ config/base.js
@@ -0,0 +1,3 @@
+module.exports = { 
+    'get_receipts_uri': 'http://localhost:8080/receipts'
+}; 
--- routes/receipt.js
+++ routes/receipt.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var axios = require('axios');
+var config = require('./../config');
+
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('receipt/index', {title: 'GRN page'});
+
+
+  var recieptsUri = config.get_receipts_uri; 
+
+  axios.get( recieptsUri )
+       .then( function(response) { 
+
+         res.render('receipt/index', {title: 'GRN listing', receipts: response.data });
+       })
+       .catch( function( error) { 
+         next(error.toString());
+       });
+
 });
 
 module.exports = router;
